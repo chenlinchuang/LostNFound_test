d7719ff768f081601d398908c86b5dc04eb30e4c
sab93667 <sab93667@gmail.com>
2021-01-19 14:44:05 +0800

    Add Router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 /* import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom"; */
-import LostForm from "./components/ImgUpload";
+import Test from "./components/test";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <LostForm />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Test />
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -1,36 +1,55 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-/* import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"; */
+import { Switch, Route, Link } from "react-router-dom";
+import LostForm from "./LostForm";
+import ImgUpload from "./ImgUpload";
+
+const Buttons = () => {
+  return (
+    <div>
+      <Link to="/lostform">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            // eslint-disable-next-line no-alert
+            alert("handleNext");
+          }}
+        >
+          LostForm
+        </Button>
+      </Link>
+      <Link to="/imgupload">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            // eslint-disable-next-line no-alert
+            alert("handleNext");
+          }}
+        >
+          ImgUpload
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
 const Test = () => {
   return (
-    <>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          // eslint-disable-next-line no-alert
-          alert("handleNext");
-        }}
-      >
-        LostForm
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          // eslint-disable-next-line no-alert
-          alert("handleNext");
-        }}
-      >
-        ImgUpload
-      </Button>
-    </>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Buttons />
+        </Route>
+        <Route path="/lostform">
+          <LostForm />
+        </Route>
+        <Route path="/imgupload">
+          <ImgUpload />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
