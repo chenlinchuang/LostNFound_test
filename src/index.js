1ce6d88350cb70eed656efa9b3d6deba3d07d9fb
ChiangHaoWei <b08901065@ntu.edu.tw>
2021-01-21 20:11:03 +0800

    revise url

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 
-const httpLink = new HttpLink({ uri: "http://localhost:4000/" });
+const httpLink = new HttpLink({ uri: "http://https://lost-and-found-group14-backend.herokuapp.com/:4000/" });
 
 const client = new ApolloClient({
   link: httpLink,
