1a6f21cada07bfa376fa6cd9371aaf88546db3e7
ChiangHaoWei <b08901065@ntu.edu.tw>
2021-01-21 17:58:05 +0800

    revise server

--- frontend/server.js
+++ frontend/server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
 const port = process.env.PORT || 80;
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-const bodyParser = require('body-parser')
-const apiRoute = require('./src/route/api');
-app.use('/api', apiRoute);
+// const bodyParser = require("body-parser");
+// const apiRoute = require("./src/route/api");
+
+app.use("/api", apiRoute);
 app.use(bodyParser.json());
 
-app.get('/ping', function (req, res) {
-  return res.send('pong');
+app.get("/ping", function (req, res) {
+  return res.send("pong");
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
-console.log("Server Ready!")
+console.log("Server Ready!");
