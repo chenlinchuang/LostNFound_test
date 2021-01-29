37086ba44cc0e28e207b4aaf0d81772e8f649fb9
chenlinchuang <b08901010@ntu.edu.tw>
2021-01-29 08:15:21 +0800

    Post detail

--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -3,10 +3,23 @@ import Button from "@material-ui/core/Button";
 import { Switch, Route, Link } from "react-router-dom";
 import LostForm from "./LostForm";
 import ImgUpload from "./ImgUpload";
+import PostIndex from "../mini_components/index";
 
 const Buttons = () => {
   return (
     <div>
+      <Link to="/postindex">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            // eslint-disable-next-line no-alert
+            alert("handleNext");
+          }}
+        >
+          PostIndex
+        </Button>
+      </Link>
       <Link to="/lostform">
         <Button
           variant="contained"
@@ -44,6 +57,9 @@ const Test = () => {
         </Route>
         <Route path="/lostform">
           <LostForm />
+        </Route>
+        <Route path="/postindex">
+          <PostIndex />
         </Route>
         <Route path="/imgupload">
           <ImgUpload />
